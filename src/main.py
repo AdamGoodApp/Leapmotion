6b9b098d08fe09f7575728dc6f93ef7432e1ffdb
Adam <adamgoodapp@gmail.com>
2014-08-31 03:04:28 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.insert(0, "../lib")
+import Leap
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+class SampleListener(Leap.Listener):
+
+  def on_connect(self, controller):
+    print "Connected"
+    controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+
+  def on_frame(self, controller):
+    frame = controller.frame()
+    print len(frame.hands)
+
+def main():
+  listener = SampleListener()
+  controller = Leap.Controller()
+
+  controller.add_listener(listener)
+
+  # Keep this process running until Enter is pressed
+  print "Press Enter to quit..."
+  try:
+    sys.stdin.readline()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    controller.remove_listener(listener)
+
+if __name__ == "__main__":
+  main()
